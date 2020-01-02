a24ce665a58eff729ee4fedf1aabd8d023a9a591
xilongxin <xilongxin@weigankeji.com>
2020-01-02 11:10:09 +0800

    生成requirements.txt

--- mobile/account/urls.py
+++ mobile/account/urls.py
@@ -3,11 +3,7 @@ from django.conf.urls import url
 from online.account import views
 
 urlpatterns = [
-    url(r'^api/phone/account/$', views.LoginView.as_view()),#用户登录,注册
-    url(r'^api/phone/user/$',views.UserView.as_view()),
-    url(r'^address/$',views.MyAddressView.as_view()),
-    url(r'^api/address/$',views.MyAddressView.as_view()),
-    url(r'^api/account/logout/$',views.LogoutView.as_view()),
-    url(r'^api/account/signup/$',views.SignUpView.as_view()),
-    url(r'^account/signup/$',views.SignUpTemplateView.as_view())
+    url(r'^api/mobile/accounts/$', views.LoginView.as_view()),  # 用户登录,注册
+    url(r'^api/mobile/users/$', views.UserView.as_view()),  # 获取/修改用户信息，地址
+    url(r'^api/account/logout/$', views.LogoutView.as_view()),  # 用户退出登录（手机端暂未使用）
 ]
--- mobile/cart/urls.py
+++ mobile/cart/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url
 from online.cart import views
 
 urlpatterns = [
-    url(r'^carts/',views.CartsView.as_view()),
-    url(r'^api/carts/',views.CartsView.as_view()),
-    url(r'^api/cart/(?P<cart_id>\d+)/$',views.CartView.as_view()),
+    url(r'^api/mobile/carts/', views.CartsView.as_view()),  # 新增，获取购物车列表
+    url(r'^api/mobile/cart/(?P<cart_id>\d+)/$', views.CartView.as_view()),  # 修改，删除购物车记录
 ]
