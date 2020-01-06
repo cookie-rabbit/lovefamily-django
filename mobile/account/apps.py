4f378292c1ea5eac6a1e2143f4a87e55775248c3
xilongxin <xilongxin@weigankeji.com>
2020-01-06 11:24:35 +0800

    管理员独立建表，将用户和管理员数据初始化

--- mobile/account/apps.py
+++ mobile/account/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class AccountConfig(AppConfig):
-    name = 'online.account'
+    name = 'mobile.account'
--- mobile/cart/apps.py
+++ mobile/cart/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class CartConfig(AppConfig):
-    name = 'online.cart'
+    name = 'mobile.cart'
